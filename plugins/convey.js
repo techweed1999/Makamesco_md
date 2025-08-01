dd6ddf5ff506fe3d5201ec33fbabd8bec6b220b6
sesco001 <marcasmatheka@gmail.com>
2025-08-02 00:01:21 +0300

    Update convey.js

--- plugins/convey.js
+++ plugins/convey.js
@@ -252,7 +252,7 @@ ezra({ nomCom: "words", categorie: "Conversion", reaction: "😂" }, async (orig
     // Create the sticker
     const stickerMess = new Sticker(meme, {
       pack: nomAuteurMessage,
-      author: 'FREDI TECH',
+      author: 'SESCO TECH',
       type: StickerTypes.FULL,
       categories: ["🤩", "🎉"],
       id: "12345",
